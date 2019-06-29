d1a3347a8ae5b3f05ed687fabe0ff44a7a0eff4a
Agustinus Theodorus <agustinustheoo@gmail.com>
2019-06-30 03:56:05 +0700

    trying to fix csrf error

--- filterest/settings.py
+++ filterest/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+CSRF_COOKIE_SECURE = False
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
